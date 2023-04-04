98ebf48225ff82be2868691d6abce658bef2c44a
Seamonster541 <se_eeds@yahoo.com>
2023-04-05 00:04:07 +0700

    Added config.ru, controllers, models(not finished)

---
+++ config.ru
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+requre './app/controllers/app'
+run Online_Checking::Api.freeze.app
---
+++ models/document.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'base64'
+require 'rbnacl'
+
+module Online_Checking
+    STORE_DIR = 'app/db/store'
