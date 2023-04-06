05244de42c2c6992ca0ddb5a05ef5e7631e98833
Borhan Lee <BorhanLee10@gmail.com>
2023-04-06 17:23:23 +0800

    rubocop and error fixed

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,62 +4,62 @@ require 'json'
 require 'base64'
 require 'rbnacl'
 
-module Online_Checking
-    STORE_DIR = 'app/db/store'
+module OnlineChecking
+  STORE_DIR = 'app/db/store'
 
-    # Holds a full secret document
-    class Document
-        # Create a new document by passing in hash of attributes
-        def initialize(new_document)
-            @id = new_document['id'] || new_id
-            @filename = new_document['filename']
-            @description = new_document['description']
-            @content = new document['content']
-        end
-
-        attr_reader :id, :filename, :description, :content
-
-        def to_json(options = {})
-            JSON(
-                {
-                    type: 'document',
-                    id: @id,
-                    filename: @filename,
-                    description: @description,
-                    content: @content
-                },
-                options
-            )
-        end
-
-        # File store must be setup once when application runs
-        def self.setup
-            Dir.mkdir(Online_Checking::STORE_DIR) unless Dir.exist? Online_Checking::STORE_DIR
-        end
-
-        # Stores document in file store
-        def save
-            File.write("#{Online_Checking::STORE_DIR}/#{id}.txt", to_json)
-        end
-
-        # Query method to find one document
-        def self.find(find_id)
-            document_file = File.read("#{Online_Checking::STORE_DIR}/#{find_id}.txt")
-            Document.new JSON.parse(document_file)
-        end
-        
-        # Query method to retrieve index of all documents
-        def self.all
-            Dir.glob("#{Online_Checking:STORE_DIR}/*.text").map do |file|
-                file.match(%r{#{Regexp.quote(Online_Checking::STORE_DIR)}/(.*)\.text})[1]
-            end
-        end
-
-        private
-
-        def new_id
-            timestamp = Time.now.to_f.to_json
-            Base64.urlsafe_encode64(RbNaCl::Hash.sha256(timestamp))[0..9]
-        end
+  # Holds a full secret document
+  class Document
+    # Create a new document by passing in hash of attributes
+    def initialize(new_document)
+      @id = new_document['id'] || new_id
+      @filename = new_document['filename']
+      @description = new_document['description']
+      @content = new_document['content']
     end
+
+    attr_reader :id, :filename, :description, :content
+
+    def to_json(options = {})
+      JSON(
+        {
+          type: 'document',
+          id: @id,
+          filename: @filename,
+          description: @description,
+          content: @content
+        },
+        options
+      )
+    end
+
+    # File store must be setup once when application runs
+    def self.setup
+      Dir.mkdir(OnlineChecking::STORE_DIR) unless Dir.exist? OnlineChecking::STORE_DIR
+    end
+
+    # Stores document in file store
+    def save
+      File.write("#{OnlineChecking::STORE_DIR}/#{id}.txt", to_json)
+    end
+
+    # Query method to find one document
+    def self.find(find_id)
+      document_file = File.read("#{OnlineChecking::STORE_DIR}/#{find_id}.txt")
+      Document.new JSON.parse(document_file)
+    end
+
+    # Query method to retrieve index of all documents
+    def self.all
+      Dir.glob("#{OnlineChecking::STORE_DIR}/*.txt").map do |file|
+        file.match(%r{#{Regexp.quote(OnlineChecking::STORE_DIR)}/(.*)\.txt})[1]
+      end
+    end
+
+    private
+
+    def new_id
+      timestamp = Time.now.to_f.to_json
+      Base64.urlsafe_encode64(RbNaCl::Hash.sha256(timestamp))[0..9]
+    end
+  end
 end
