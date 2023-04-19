b39e6fdd8d853b4df3ccc9f29ff7b6a924bc2e07
Seamonster541 <se_eeds@yahoo.com>
2023-04-19 11:19:14 +0800

    added uuid to documents, added new rake function newkey

--- app/db/migrations/002_documents_create.rb
+++ app/db/migrations/002_documents_create.rb
@@ -5,7 +5,7 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:documents) do
-      primary_key :id
+      uuid: :id, primary_key true
       foreign_key :household_id, table: :households
 
       String :filename, null: false
--- config/environments.rb
+++ config/environments.rb
@@ -6,34 +6,44 @@ require 'logger'
 require 'sequel'
 require './app/lib/secure_db'
 
-
 module OnlineCheckIn
   # Configuration for the API
   class Api < Roda
     plugin :environments
+
     # rubocop:disable Lint/ConstantDefinitionInBlock
     configure do
-    # load config secrets into local environment variables (ENV)
+      # load config secrets into local environment variables (ENV)
       Figaro.application = Figaro::Application.new(
         environment: environment, # rubocop:disable Style/HashSyntax
         path: File.expand_path('config/secrets.yml')
       )
       Figaro.load
 
-    # Make the environment variables accessible to other classes
-    def self.config
-       Figaro.env
-    end 
+      # Make the environment variables accessible to other classes
+      def self.config
+        Figaro.env
+      end
 
-    # Connect and make the database accessible to other classes
-    db_url = ENV.delete('DATABASE_URL')
-    DB = Sequel.connect("#{db_url}?encoding=utf8")
-    def self.DB
-      DB # rubocop:disable Naming/MethodName
-       # HTTP Request logging
-      configure :development, :production do
-        plugin :common_logger, $stdout
+      # Connect and make the database accessible to other classes
+      db_url = ENV.delete('DATABASE_URL')
+      DB = Sequel.connect("#{db_url}?encoding=utf8")
+      def self.DB
+        DB
+        # HTTP Request logging
+        configure :development, :production do
+          plugin :common_logger, $stdout
+        end
+      end
+
+      #LOGGER = Logger.new($stderr)
+      #def self.logger
+      #  LOGGER
+      #end
+
+      SecureDB.setup(ENV.delete('DB_KEY'))
     end
+
     configure :development, :test do
       require 'pry'
       logger.level = Logger::ERROR
