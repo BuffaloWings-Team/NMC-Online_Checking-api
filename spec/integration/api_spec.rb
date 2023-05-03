477673af3df6b25fa4a3d4f3f8ad42949e0385ef
Borhan Lee <BorhanLee10@gmail.com>
2023-05-04 00:42:06 +0800

    add dropAll to delete all db files

--- spec/integration/api_spec.rb
+++ spec/integration/api_spec.rb
@@ -3,10 +3,16 @@
 require_relative '../spec_helper'
 
 describe 'Test OnlineCheckIn Web API' do
+
+  before do
+    Dir.glob('app/db/store/*.txt').each { |filename| FileUtils.rm(filename) }
+  end
+    
   describe 'Root route' do
     it 'should find the root route' do
       get '/'
       _(last_response.status).must_equal 200
     end
   end
+
 end
