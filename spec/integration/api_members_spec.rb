4d477bb021457f9807cbbd0d23507d58fcd69471
Seamonster541 <se_eeds@yahoo.com>
2023-05-04 19:17:44 +0800

    changed count from 4 to 2 in member data

--- spec/integration/api_members_spec.rb
+++ spec/integration/api_members_spec.rb
@@ -23,7 +23,7 @@ describe 'Test Member Handling' do
     _(last_response.status).must_equal 200
 
     result = JSON.parse(last_response.body)['data']
-    _(result.count).must_equal 4
+    _(result.count).must_equal 2
     result.each do |member|
       _(member['type']).must_equal 'member'
     end
