a2fd687ead80ad68958058726ce2588b49c39bce
Seamonster541 <se_eeds@yahoo.com>
2023-05-04 18:51:37 +0800

    return deleted code from api_spec

--- spec/integration/api_spec.rb
+++ spec/integration/api_spec.rb
@@ -4,6 +4,10 @@ require_relative '../spec_helper'
 
 describe 'Test OnlineCheckIn Web API' do
   describe 'Root route' do
+    before do
+      Dir.glob('app/db/store/*.txt').each { |filename| FileUtils.rm(filename) }
+    end
+    
     it 'should find the root route' do
       get '/'
       _(last_response.status).must_equal 200
