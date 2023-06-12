cfb8bb8cb9508f01d906fd582e0bfd03c5099bba
Seamonster541 <se_eeds@yahoo.com>
2023-06-12 12:25:29 +0800

    fixed issues, will need modification of yml file to represent ours

--- spec/integration/api_auth_spec.rb
+++ spec/integration/api_auth_spec.rb
@@ -71,7 +71,7 @@ describe 'Test Authentication Routes' do
     end
 
     it 'HAPPY AUTH SSO: should authorize existing SSO account' do
-      Credence::Account.create(
+      OnlineCheckIn::Account.create(
         username: SSO_ACCOUNT['sso_username'],
         email: SSO_ACCOUNT['email']
       )
